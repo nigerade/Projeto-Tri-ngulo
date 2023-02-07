abfc736719f1e644ffa93709d9f47e75435dcd53
Nicholas Gerade <64471143+nigerade@users.noreply.github.com>
2023-02-07 12:15:44 -0300

    Add files via upload

---
+++ Triangulo/Triangulo/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Triangulo triangulo = new Triangulo();
+            int op = 0;
+
+            do
+            {
+                
+
+                Console.WriteLine("Selecione uma Opção:\n" +
+                    "1- Iniciar\n2- Fechar Programa");
+                op = int.Parse(Console.ReadLine());
+
+                Console.Clear();
+
+                switch (op)
+                {
+                    case 1:
+                        Console.WriteLine("Lado 1 do Triângulo: ");
+                        triangulo.lado1 = double.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Lado 2 do Triângulo: ");
+                        triangulo.lado2 = double.Parse(Console.ReadLine());
+
+
+                        Console.WriteLine("Lado 3 do Triângulo: ");
+                        triangulo.lado3 = double.Parse(Console.ReadLine());
+                        break;
+
+                    case 2:
+                        Environment.Exit(0);
+                        break;
+
+                }
+
+                if (triangulo.IsTriangle())
+                {
+                    Console.WriteLine("A classificação é: " + triangulo.classificacao + "\n" + "Área do Triângulo: "+ triangulo.area + "\n" + "Perímetro do triângulo: " + triangulo.perimetro + "\n");
+
+                }
+                else
+                {
+                    Console.WriteLine("Triâgulo inválido!!!");
+                }
+                
+
+
+
+
+            } while (op != 2);
+        }
+      }
+ }
+
---
+++ Triangulo/Triangulo/Triangulo.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulo
+{
+    class Triangulo
+    {
+        public double lado1;
+        public double lado2;
+        public double lado3;
+        public String classificacao;
+        public double perimetro;
+        public double area;
+
+        public Triangulo()
+        {
+            lado1 = 0.0;
+            lado2 = 0.0;
+            lado3 = 0.0;
+            classificacao = "";
+            perimetro = 0.0;
+            area = 0.0;
+        }
+
+        public Triangulo(
+        double Lado1,
+        double Lado2, 
+        double Lado3, 
+        string Classificao, 
+        double Perimetro, 
+        double Area)
+        
+        {
+            lado1 = Lado1;
+            lado2 = Lado2;
+            lado3 = Lado3;
+            classificacao = Classificao;
+            perimetro = Perimetro;
+            area = Area;
+        }
+
+        public Triangulo(
+            double Lado1, double Lado2, double Lado3)
+        {
+            lado1 = Lado1;
+            lado2 = Lado2;
+            lado3 = Lado3;
+        }
+
+        public Boolean IsTriangle ()
+        {
+            if (lado1 < lado2 + lado3 && lado2 < lado1 + lado3 && lado3 < lado1 + lado2)
+            {
+                Classificar();
+                CalcularArea();
+                return true;
+
+            }
+            return false;
+        }
+
+        public void CalcularPerimetro()
+        {
+            perimetro = lado1 + lado2 + lado3;
+        }
+
+        public void Classificar()
+        {
+            if (lado1 == lado2 && lado2 == lado3)
+            {
+                classificacao = "Equilátero";
+            }
+            else if (lado1 == lado2 || lado2 == lado3 || lado1 == lado3)
+            {
+                classificacao = "Isósceles";
+            }
+            else{
+                classificacao = "Escaleno";
+            }
+        }
+
+        public void CalcularArea()
+        {
+            CalcularPerimetro();
+           double sp = perimetro / 2;
+           area= Math.Sqrt(sp * (sp - lado1) * (sp - lado2) * (sp - lado3) ); 
+        }
+
+        
+    }
+}
